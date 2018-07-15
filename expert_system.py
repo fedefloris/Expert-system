23408de546bf6e019756f04b1a0e99b903e88e1d
davhojt <davhojt@gmail.com>
2018-07-15 21:52:37 +0300

    Now strips away unnecessary characters.

--- expert_system.py
+++ expert_system.py
@@ -6,7 +6,7 @@
 #    By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/12 00:09:39 by dhojt             #+#    #+#              #
-#    Updated: 2018/07/13 00:58:18 by dhojt            ###   ########.fr        #
+#    Updated: 2018/07/15 16:04:44 by dhojt            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ from parse import parse
 def main():
 	lines = parse()
 	for x in lines:
-		print ("Type: %d  |  String: %s" % (x.types, x.string))
+		print ("Type[%d]\nStr[%s]\nData[%s]\n\n" % (x.type, x.string, x.data))
 
 if __name__== "__main__":
 	main()
--- parse.py
+++ parse.py
@@ -6,7 +6,7 @@
 #    By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/12 20:49:23 by dhojt             #+#    #+#              #
-#    Updated: 2018/07/13 00:58:38 by dhojt            ###   ########.fr        #
+#    Updated: 2018/07/15 19:03:32 by dhojt            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,22 +25,30 @@ def read_file():
 	f.close
 	return (lines)
 
+def get_type(line):
+	if line == "\n":
+		return (4)
+	return (1)
+
 # LINE TYPES
 # 0 Error
-# 1 Conditions
-# 2 True
-# 3 Queries
+# 1 Rule
+# 2 Fact
+# 3 Query
 # 4 Blank Line
 
 def parse():
 	class Line:
 		def __init__(self, types, string):
-			self.types = types
+			self.type = types
 			self.string = string
+			string = string.replace("\t", "")
+			string = string.replace(" ", "")
+			string = string.split("#")[0]
+			self.data = string
 
 	lines = []
-	for x in read_file():
-		tmp = Line(3, x) #need to identify the correct type.
+	for line in read_file():
+		tmp = Line(get_type(line), line.replace("\n", ""))
 		lines.append(tmp)
-
 	return (lines)
