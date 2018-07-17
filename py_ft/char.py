dbd6834d0cfd41de93297ea053d6b94bac71163d
davhojt <davhojt@gmail.com>
2018-07-17 10:47:37 +0300

    Added is_digit

--- py_ft/char.py
+++ py_ft/char.py
@@ -6,10 +6,11 @@
 #    By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/16 19:48:33 by dhojt             #+#    #+#              #
-#    Updated: 2018/07/16 19:48:42 by dhojt            ###   ########.fr        #
+#    Updated: 2018/07/17 10:45:54 by dhojt            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+# is_upper tests if single character is lower case
 def is_upper(c):
 	if c >= "A" and c <= "Z":
 		return (1)
@@ -19,6 +20,13 @@ def is_upper(c):
 # is_lower tests if single character is lower case
 def is_lower(c):
 	if c >= "a" and c <= "z":
+		return (1)
+	return (0)
+
+
+# is_digit tests if single character is a number
+def is_digit(c):
+	if c >= "0" and c <= "9":
 		return (1)
 	return (0)
 
