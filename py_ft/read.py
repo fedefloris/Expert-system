1c403ba259b400f9935fcd0b7a62a1c1eb2b5350
davhojt <davhojt@gmail.com>
2018-07-17 09:38:01 +0300

    Modified read_lines to read one line at a time. To open up protection against alrge files.

--- py_ft/read.py
+++ py_ft/read.py
@@ -6,18 +6,19 @@
 #    By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/16 19:37:03 by dhojt             #+#    #+#              #
-#    Updated: 2018/07/17 09:16:30 by dhojt            ###   ########.fr        #
+#    Updated: 2018/07/17 09:37:13 by dhojt            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # read_lines reads file and returns all lines in an array
 def read_lines(file_name):
+
+	lines = []
 	try:
-		f = open(file_name, "r")
+		with open(file_name) as read:
+			for line in read:
+				lines.append(line)
 	except IOError:
 		return (0)
-	if f.mode != "r":
-		return (0)
-	lines = f.readlines()
-	f.close
+
 	return (lines)
