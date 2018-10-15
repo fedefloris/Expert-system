ed69b9d44f06944983748c838bf58b58e9b4c0ef
Federico Floris <fzfloris@gmail.com>
2018-10-15 14:21:36 +0200

    Fixed several bugs inside get_token_from_expr

--- src/parser/Parser.py
+++ src/parser/Parser.py
@@ -22,10 +22,10 @@ from LineLexer import LineLexer
 class Parser:
 	def __init__(self, config):
 		self.config = config
-		self.__create_operations()
-		self.__create_tokens()
+		self._create_operations()
+		self._create_tokens()
 
-	def __create_operations(self):
+	def _create_operations(self):
 		# Ordered by increasing priority
 		self.operations = [
 			(self.config.op_xor, Xor),
@@ -34,35 +34,50 @@ class Parser:
 			(self.config.op_not, Not)
 		]
 
-	def __create_tokens(self):
+	def _create_tokens(self):
+		lines_to_remove = []
 		for line in self.config.lines:
 			if line.type == LineLexer.RULE_TYPE:
-				self.__create_token(line)
+				if self.config.bicondition_sub in line.data:
+					new = LineLexer(self.config, "", len(self.config.lines) + 1)
+					lines_to_remove.append(line)
+					self.config.lines.append(new)
+				else:
+					self._create_token(line)
+		for line in lines_to_remove:
+			self.config.lines.remove(line)
 
+	def _create_token(self, line):
+		expr = self.config.left_bracket
+		expr += line.data.split(self.config.implies_sub)[0]
+		expr += self.config.right_bracket
+		line.token = self._get_token_from_expr(expr)
 
-	def __create_token(self, line):
-		expr = line.data.split(self.config.implies_sub)[0]
-		line.token = self.__get_token_from_expr(expr)
-
-	def __get_token_from_expr(self, expr):
-		# Remove brackets both at the begin and at the end
-		if len(expr) > 1 and expr[0] == self.config.left_bracket:
+	def _get_token_from_expr(self, expr):
+		if self._brackets_at_the_edges(expr):
 			expr = expr[1:-1]
 		if len(expr) <= 1:
 			return Base(expr)
 		for op, op_token in self.operations:
-			insideBrackets = False
-			for c in expr:
+			inside_brackets = False
+			for i, c in enumerate(expr):
 				if c == self.config.left_bracket:
-					insideBrackets = True
+					inside_brackets = True
 				elif c == self.config.right_bracket:
-					insideBrackets = False
-				elif insideBrackets == False and c == op:
-					split = expr.split(op, 1)
-					new = op_token("")
+					inside_brackets = False
+				elif not inside_brackets and c == op:
+					new = op_token(expr)
 					if (op_token != Not):
-						new.add_true(self.__get_token_from_expr(split[0]))
-						new.add_true(self.__get_token_from_expr(split[1]))
+						new.add_true(self._get_token_from_expr(expr[0:i]))
+						new.add_true(self._get_token_from_expr(expr[i+1:]))
 					else:
-						new.add_true(self.__get_token_from_expr(split[1]))
+						new.add_true(self._get_token_from_expr(expr[i+1:]))
 					return new
+		return self._get_token_from_expr(expr)
+
+	def _brackets_at_the_edges(self, expr):
+		if len(expr) <= 1:
+			return False
+		if expr[0] != self.config.left_bracket or expr[-1] != self.config.right_bracket:
+			return False
+		return True
