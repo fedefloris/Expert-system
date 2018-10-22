378c4c686f7059f73b147cddb095a6250b7dea80
Federico Floris <fzfloris@gmail.com>
2018-10-22 17:42:13 +0200

    Changed few things such as the SyntaxError format

--- src/parser/Lexer.py
+++ src/parser/Lexer.py
@@ -25,8 +25,7 @@ class Lexer:
 		line_read = Reader(file_name, self.config.max_lines).lines
 		if not line_read:
 			raise ParsingError(f"\033[1;31mRead error\033[1;37m: {file_name}")
-		self.lines = [LineLexer(self.config, line)
-			for line in line_read]
+		self.lines = [LineLexer(self.config, line) for line in line_read]
 		self.config.lines = self.lines
 
 	def _check_errors(self):
@@ -34,7 +33,7 @@ class Lexer:
 		for line_num, line in enumerate(self.lines):
 			if line.type == LineLexer.ERROR_TYPE:
 				errors.append("\033[1;31mError:\033[1;37m "
-					f"Invalid syntax on line \033[1;34m{line_num}\033[1;32m\n\t"
+					f"Invalid syntax on line \033[1;34m{line_num + 1}\033[1;32m\n\t"
 					f"\"\033[1;37m{line.string}\033[1;32m\"\033[1;37m")
 				errors.append("\n")
 		if len(errors) > 0:
